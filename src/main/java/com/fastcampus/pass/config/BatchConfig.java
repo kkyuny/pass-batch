a50bba6e4a3b959c37ecde74613126ad3f5ac486
kyun <kyun9911@gmail.com>
2025-10-03 18:21:08 +0900

    [상세기능 구현] 기본 앤티티, 레포지토리, 컨피그 파일 추가

---
+++ src/main/java/com/fastcampus/pass/config/BatchConfig.java
@@ -0,0 +1,31 @@
+package com.fastcampus.pass.config;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+public class BatchConfig {
+    @Bean
+    public Job sampleJob(JobRepository jobRepository, Step sampleStep) {
+        return new JobBuilder("sampleJob", jobRepository)
+                .start(sampleStep)
+                .build();
+    }
+
+    @Bean
+    public Step sampleStep(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+        return new StepBuilder("sampleStep", jobRepository)
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println(">>> Sample Step Executed!");
+                    return RepeatStatus.FINISHED;
+                }, transactionManager)
+                .build();
+    }
+}
